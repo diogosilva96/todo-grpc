c1f5aa7498acf08716b461eb81acfe2e9487c2ea
Diogo Silva <diogo_silva_96@hotmail.com>
2023-08-07 21:35:51 +0100

    Fix DI

--- src/Todo.Grpc/Program.cs
+++ src/Todo.Grpc/Program.cs
@@ -1,13 +1,15 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Todo.Grpc.Data;
 using Todo.Grpc.Services;
+using Todo.Grpc.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-       .AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=TodoDatabase"));
-
-builder.Services
+       .AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=TodoDatabase"))
+       .AddValidatorsFromAssemblyContaining<Program>()
+       .AddTransient<IGrpcRequestValidator, GrpcRequestValidator>()
        .AddGrpc()
        .AddJsonTranscoding();
 
@@ -15,6 +17,8 @@ var app = builder.Build();
 
 app.MapGrpcService<TodoService>();
 
-app.MapGet("/", () =>"Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
