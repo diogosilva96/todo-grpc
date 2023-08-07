e2d8b7ba194f29a7a27fbbbc6c22b70749da240f
Diogo Silva <diogo_silva_96@hotmail.com>
2023-08-07 21:35:08 +0100

    Add RpcExceptions

---
+++ src/Todo.Grpc/Services/RpcExceptions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Results;
+using Grpc.Core;
+
+namespace Todo.Grpc.Services;
+
+public static class RpcExceptions
+{
+    public static RpcException NotFound() => new(new(StatusCode.NotFound, "The object could not be found."));
+
+    public static RpcException BadRequest(ValidationResult validationResult) =>
+        // TODO: find better way to do (e.g., maybe via metadata)
+        new(new(StatusCode.InvalidArgument,
+            $"The object is not valid, details: {string.Join(", ", validationResult.Errors.Select(x => $"{x.PropertyName}: {x.ErrorMessage}"))}"));
+}
