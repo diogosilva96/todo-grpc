90e8416b7f34e09d699c3b750ce2890a260da4f0
Diogo Silva <diogo_silva_96@hotmail.com>
2023-08-07 21:49:57 +0100

    Add ResponseMapper

---
+++ src/Todo.Grpc/Services/ResponseMapper.cs
@@ -0,0 +1,41 @@
+﻿using Todo.Grpc.Models;
+
+namespace Todo.Grpc.Services;
+
+public static class ResponseMapper
+{
+    public static UpdateTodoResponse MapToUpdateTodoResponse(TodoItem todo) => new()
+    {
+        Id = todo.Id
+    };
+
+    public static DeleteTodoResponse MapToDeleteTodoResponse(TodoItem todo) => new()
+    {
+        Id = todo.Id
+    };
+
+    public static GetTodoResponse MapToGetTodoResponse(TodoItem todo) => new()
+    {
+        Id = todo.Id,
+        Description = todo.Description,
+        Status = todo.TodoStatus,
+        Title = todo.Title
+    };
+
+    public static ListTodoResponse MapToListTodoResponse(IEnumerable<TodoItem> todos)
+    {
+        var listResponse = new ListTodoResponse();
+
+        foreach (var todo in todos)
+        {
+            listResponse.ToDo.Add(MapToGetTodoResponse(todo));
+        }
+
+        return listResponse;
+    }
+
+    public static CreateTodoResponse MapToCreateTodoResponse(TodoItem todo) => new()
+    {
+        Id = todo.Id
+    };
+}
--- src/Todo.Grpc/Services/TodoService.cs
+++ src/Todo.Grpc/Services/TodoService.cs
@@ -21,19 +21,16 @@ public class TodoService : Todo.TodoBase
     {
         await _requestValidator.ValidateAndThrowIfInvalidAsync(request, context.CancellationToken);
 
-        var todoItem = new TodoItem
+        var todo = new TodoItem
         {
             Title = request.Title,
             Description = request.Description
         };
 
-        await _dbContext.AddAsync(todoItem, context.CancellationToken);
+        await _dbContext.AddAsync(todo, context.CancellationToken);
         await _dbContext.SaveChangesAsync(context.CancellationToken);
 
-        return new()
-        {
-            Id = todoItem.Id
-        };
+        return ResponseMapper.MapToCreateTodoResponse(todo);
     }
 
     public override async Task<GetTodoResponse> GetTodo(GetTodoRequest request, ServerCallContext context)
@@ -46,32 +43,13 @@ public class TodoService : Todo.TodoBase
             throw RpcExceptions.NotFound();
         }
 
-        return new()
-        {
-            Id = todo.Id,
-            Description = todo.Description,
-            Status = todo.TodoStatus,
-            Title = todo.Title
-        };
+        return ResponseMapper.MapToGetTodoResponse(todo);
     }
 
     public override async Task<ListTodoResponse> ListTodo(ListTodoRequest request, ServerCallContext context)
     {
         var todos = await _dbContext.TodoItems.ToArrayAsync(context.CancellationToken);
-        var listResponse = new ListTodoResponse();
-
-        foreach (var todo in todos)
-        {
-            listResponse.ToDo.Add(new GetTodoResponse
-            {
-                Id = todo.Id,
-                Description = todo.Description,
-                Status = todo.TodoStatus,
-                Title = todo.Title
-            });
-        }
-
-        return listResponse;
+        return ResponseMapper.MapToListTodoResponse(todos);
     }
 
     public override async Task<UpdateTodoResponse> UpdateTodo(UpdateTodoRequest request, ServerCallContext context)
@@ -89,10 +67,7 @@ public class TodoService : Todo.TodoBase
         todo.Title = request.Title;
 
         await _dbContext.SaveChangesAsync(context.CancellationToken);
-        return new()
-        {
-            Id = todo.Id
-        };
+        return ResponseMapper.MapToUpdateTodoResponse(todo);
     }
 
     public override async Task<DeleteTodoResponse> DeleteTodo(DeleteTodoRequest request, ServerCallContext context)
@@ -109,9 +84,6 @@ public class TodoService : Todo.TodoBase
 
         await _dbContext.SaveChangesAsync(context.CancellationToken);
 
-        return new()
-        {
-            Id = todo.Id
-        };
+        return ResponseMapper.MapToDeleteTodoResponse(todo);
     }
 }
