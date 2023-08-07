a0f3af75bb51b29e95e60b55b2bb7a7fba602ace
Diogo Silva <diogo_silva_96@hotmail.com>
2023-08-07 20:23:49 +0100

    Code cleanup

--- src/Todo.Grpc/Data/AppDbContext.cs
+++ src/Todo.Grpc/Data/AppDbContext.cs
@@ -6,9 +6,7 @@ namespace Todo.Grpc.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-        
-    }
+    { }
 
     public DbSet<TodoItem> TodoItems => Set<TodoItem>();
 }
