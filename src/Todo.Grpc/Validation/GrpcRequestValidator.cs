67a73731d3b006d6bef211d494893cc7c7795824
Diogo Silva <diogo_silva_96@hotmail.com>
2023-08-07 21:35:17 +0100

    Add GrpcRequestValidator

---
+++ src/Todo.Grpc/Validation/GrpcRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Todo.Grpc.Services;
+
+namespace Todo.Grpc.Validation;
+
+public class GrpcRequestValidator : IGrpcRequestValidator
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public GrpcRequestValidator(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+    public async Task ValidateAndThrowIfInvalidAsync<T>(T request,
+        CancellationToken cancellationToken = default)
+    {
+        var validator = GetValidatorOrThrow<T>();
+
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (validationResult.IsValid) return;
+
+        throw RpcExceptions.BadRequest(validationResult);
+    }
+
+    private IValidator<T> GetValidatorOrThrow<T>()
+    {
+        var validator = _serviceProvider.GetService<IValidator<T>>();
+        if (validator == default)
+        {
+            throw new InvalidOperationException($"Could not find validator for object of type '{typeof(T)}'");
+        }
+
+        return validator;
+    }
+}
---
+++ src/Todo.Grpc/Validation/IGrpcRequestValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace Todo.Grpc.Validation;
+
+public interface IGrpcRequestValidator
+{
+    public Task ValidateAndThrowIfInvalidAsync<T>(T request, CancellationToken cancellationToken = default);
+}
